ba05735673476f4716fdb5d11df2ee8f898001d7
l-lini <119787571+l-lini@users.noreply.github.com>
2026-09-11 19:39:40 +0200

    git: nice defaults for my workflow

--- os/git.nix
+++ os/git.nix
@@ -2,7 +2,6 @@
 
 {
   environment.shellAliases = {
-    g = "git";
     s = "git status";
     c = "git commit";
     a = "git add";
@@ -10,12 +9,33 @@
     u = "git pull";
     d = "git diff";
     l = "git log";
+    restore = "git restore";
+    switch = "git switch";
+    checkout = "git checkout -b";
+    merge = "git merge --squash";
+    clone = "git clone";
   };
 
   programs.git = {
     enable = true;
     config = {
       init.defaultBranch = "main";
+      branch.main.mergeOptions = "--squash";
+      push = {
+        default = "simple";
+        # autoSetupRemote = true;
+      };
+      pull = {
+        rebase = true;
+        autoStash = true;
+      };
+      url = {
+        "https://github.com/" = {
+          insteadOf = [
+            "github:"
+          ];
+        };
+      };
     };
   };
 }
